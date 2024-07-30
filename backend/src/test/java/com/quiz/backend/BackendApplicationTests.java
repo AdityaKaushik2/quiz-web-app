b2ea879a24f5e81f18ed36c4fb45863e643e9622
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-07-30 12:55:34 +0530

    project setup

---
+++ backend/src/test/java/com/quiz/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+
+  return (
+    <>
+        <h1 className="underline font-bold text-center content-center mt-80">React App</h1>
+    </>
+  )
+}
+export default App
