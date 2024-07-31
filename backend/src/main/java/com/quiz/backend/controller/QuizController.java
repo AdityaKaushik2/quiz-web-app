eef08d63b1589590f50758a7aee6b20b120b1925
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-01 00:44:43 +0530

    Added endpoint to get quizzes as per user id

--- backend/src/main/java/com/quiz/backend/controller/QuizController.java
+++ backend/src/main/java/com/quiz/backend/controller/QuizController.java
@@ -1,2 +1,26 @@
-package com.quiz.backend.controller;public class QuizController {
+package com.quiz.backend.controller;
+
+import com.quiz.backend.entity.Quiz;
+import com.quiz.backend.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class QuizController {
+    private final QuizService quizService;
+
+    @Autowired
+    public QuizController(QuizService quizService){
+        this.quizService = quizService;
+    }
+
+    @GetMapping("/quiz/{id}")
+    public List<Quiz> getQuizList(@PathVariable Long id){
+        return quizService.getAllQuiz(id);
+    }
 }
