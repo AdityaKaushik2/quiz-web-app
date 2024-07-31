9035a6a43853c90bdfcd85f2d6ede6ffdb86c0b5
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-07-31 16:35:53 +0530

    Implement getUser method to fetch a User from repository

--- backend/src/main/java/com/quiz/backend/service/UserService.java
+++ backend/src/main/java/com/quiz/backend/service/UserService.java
@@ -3,9 +3,12 @@ package com.quiz.backend.service;
 import com.quiz.backend.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
     List<User> getAllUsers();
 
     User saveUser(User newUser);
+
+    Optional<User> getUser(Long id);
 }
--- backend/src/main/java/com/quiz/backend/service/UserServiceImpl.java
+++ backend/src/main/java/com/quiz/backend/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -25,5 +26,10 @@ public class UserServiceImpl implements UserService{
     @Override
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUser(Long id) {
+        return userRepository.findById(id);
     }
 }
