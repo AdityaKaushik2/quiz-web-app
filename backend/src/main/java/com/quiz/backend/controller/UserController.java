6bff5d4a69a0eabaa52598ff9e04b2953ddc1df1
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-07-31 12:19:12 +0530

    Added endpoint to retrieve all users from the repository

--- backend/src/main/java/com/quiz/backend/controller/UserController.java
+++ backend/src/main/java/com/quiz/backend/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.quiz.backend.controller;
 
+import com.quiz.backend.entity.User;
 import com.quiz.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -10,7 +14,12 @@ public class UserController {
     private final UserService userService;
 
     @Autowired
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 }
