661ff0104ee062dac6ef9cc4d7d197eb3b03b398
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-01 00:26:44 +0530

    Add findByUserId method in QuizRepository to get quizzes by user ID

--- backend/src/main/java/com/quiz/backend/repository/QuizRepository.java
+++ backend/src/main/java/com/quiz/backend/repository/QuizRepository.java
@@ -3,5 +3,8 @@ package com.quiz.backend.repository;
 import com.quiz.backend.entity.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    List<Quiz> findByUserId(Long userId);
 }
