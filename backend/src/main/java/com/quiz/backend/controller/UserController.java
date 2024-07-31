e48eea7dbe3993b8a43c9b3ede2adf19ee1288cf
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-07-31 12:11:56 +0530

    Implemented constructor-based DI for UserService in UserController

--- backend/src/main/java/com/quiz/backend/controller/UserController.java
+++ backend/src/main/java/com/quiz/backend/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.quiz.backend.controller;
 
+import com.quiz.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
 }
