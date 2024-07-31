ab83f33d12bfb82a220717e5db3ff3dabcf1254f
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-07-31 11:58:17 +0530

    Implement constructor-based d.i for UserRepository in UserService

--- backend/src/main/java/com/quiz/backend/service/UserServiceImpl.java
+++ backend/src/main/java/com/quiz/backend/service/UserServiceImpl.java
@@ -1,8 +1,16 @@
 package com.quiz.backend.service;
+import com.quiz.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import com.quiz.backend.entity.User;
-
-import java.util.List;
+@Service
 
 public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 }
