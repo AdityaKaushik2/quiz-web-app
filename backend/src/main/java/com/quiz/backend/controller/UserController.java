acd07fe2b7e696defb43306ae55d1d7664fc772c
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-07-31 13:26:01 +0530

    Added endpoint to save a user in repository

--- backend/src/main/java/com/quiz/backend/controller/UserController.java
+++ backend/src/main/java/com/quiz/backend/controller/UserController.java
@@ -4,6 +4,8 @@ import com.quiz.backend.entity.User;
 import com.quiz.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +18,11 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    User saveUser(@RequestBody User newUser){
+        return userService.saveUser(newUser);
     }
 
     @GetMapping("/users")
