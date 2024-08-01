5ada1f631435eb5feaee8683bd66a08244aa3705
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-01 12:09:53 +0530

    Refactored find ByUserId to findByUser_Id to map it correctly to Quiz Entity

--- backend/src/main/java/com/quiz/backend/repository/QuizRepository.java
+++ backend/src/main/java/com/quiz/backend/repository/QuizRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    List<Quiz> findByUserId(Long userId);
+    List<Quiz> findByUser_Id(Long userId);
 }
