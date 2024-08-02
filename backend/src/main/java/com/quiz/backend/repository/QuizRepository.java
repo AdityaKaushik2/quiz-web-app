007cae0c6c0a159ef46dfd6c825587e51159d576
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-03 01:23:26 +0530

    Add: method to get quiz by id and userId

--- backend/src/main/java/com/quiz/backend/repository/QuizRepository.java
+++ backend/src/main/java/com/quiz/backend/repository/QuizRepository.java
@@ -4,7 +4,9 @@ import com.quiz.backend.entity.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
     List<Quiz> findByUser_Id(Long userId);
+    Optional<Quiz> findByIdAndUser_Id(Long id, Long userId);
 }
