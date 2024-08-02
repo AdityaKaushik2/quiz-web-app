69aedc766f0f2441f84abb4e3e8a205e6175f6ee
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-03 00:18:13 +0530

    Add findByQuiz_Id method to Question repository

--- backend/src/main/java/com/quiz/backend/repository/QuestionRepository.java
+++ backend/src/main/java/com/quiz/backend/repository/QuestionRepository.java
@@ -3,6 +3,8 @@ package com.quiz.backend.repository;
 import com.quiz.backend.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+import java.util.List;
 
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findByQuiz_Id(Long quizId);
 }
