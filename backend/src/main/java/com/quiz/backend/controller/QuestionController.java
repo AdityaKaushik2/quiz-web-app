35f18f41a99a41171fc32abf9e9bfc8f71960bf8
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-03 00:58:30 +0530

    Added Endpoints to fetch the questions

--- backend/src/main/java/com/quiz/backend/controller/QuestionController.java
+++ backend/src/main/java/com/quiz/backend/controller/QuestionController.java
@@ -1,8 +1,13 @@
 package com.quiz.backend.controller;
 
+import com.quiz.backend.entity.Question;
 import com.quiz.backend.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class QuestionController {
@@ -12,5 +17,10 @@ public class QuestionController {
     @Autowired
     public QuestionController(QuestionService questionService){
         this.questionService = questionService;
+    }
+
+    @GetMapping("/user/{userId}/quiz/{quizId}")
+    public List<Question> getAllQuestion(@PathVariable Long quizId, @PathVariable Long userId){
+        return questionService.getAllQuestion(quizId, userId);
     }
 }
