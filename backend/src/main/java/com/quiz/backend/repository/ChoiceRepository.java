aa06077039628e81faf65c6ccedd101ed68f2494
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-03 13:09:50 +0530

    Add ChoiceRepository with methods for handling choices related to questions and users

--- backend/src/main/java/com/quiz/backend/repository/ChoiceRepository.java
+++ backend/src/main/java/com/quiz/backend/repository/ChoiceRepository.java
@@ -3,5 +3,11 @@ package com.quiz.backend.repository;
 import com.quiz.backend.entity.Choice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChoiceRepository extends JpaRepository<Choice,Long> {
+import java.util.List;
+
+public interface ChoiceRepository extends JpaRepository<Choice, Long> {
+    List<Choice> findByQuestion_Id(Long questionId);
+    List<Choice> findByQuestion_IdAndQuestion_Quiz_Id(Long questionId, Long quizId);
+    List<Choice> findByQuestion_IdAndQuestion_Quiz_IdAndQuestion_Quiz_User_Id(Long questionId, Long quizId, Long userId);
+    Choice findByIdAndQuestion_IdAndQuestion_Quiz_IdAndQuestion_Quiz_User_Id(Long choiceId, Long questionId, Long quizId, Long userId);
 }
