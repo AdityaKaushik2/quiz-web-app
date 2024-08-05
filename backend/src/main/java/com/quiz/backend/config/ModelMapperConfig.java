1010c6fe49ddec2e92533e8e76f8233d672d4dc0
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-05 23:17:51 +0530

    Configure Model Mapper

---
+++ backend/src/main/java/com/quiz/backend/config/ModelMapperConfig.java
@@ -0,0 +1,16 @@
+package com.quiz.backend.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT) ;
+        return modelMapper;
+    }
+}
