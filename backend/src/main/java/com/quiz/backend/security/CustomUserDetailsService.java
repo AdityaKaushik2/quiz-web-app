87e4c117ca5f39673c50c5f2555aca0401b8cc61
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-07 17:20:37 +0530

    Implemented UserDetailsService and loadUserByUsername Method

---
+++ backend/src/main/java/com/quiz/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.quiz.backend.security;
+
+import com.quiz.backend.entity.User;
+import com.quiz.backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new CustomUserDetails(user);
+    }
+}
