0d1f9a20c4e3540634e4d23320e2c542bb39a2f1
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-09 15:43:39 +0530

    Add UserDTO to expose limited user data via GET endpoint

---
+++ backend/src/main/java/com/quiz/backend/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.quiz.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+}
