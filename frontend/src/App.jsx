a46e368b9f4ef9378ca28ecc28879e11f893475f
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-09 17:15:32 +0530

    configured app.jsx and app.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import Footer from "./components/Footer/Footer.jsx";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/LoginSection.jsx';
+import Register from './pages/Register';
+import UserDetails from './pages/UserDetailsPage.jsx';
+import Homepage from "./components/Homepage.jsx"; // Ensure this is the correct path
 
-function App() {
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/user-details/:username" element={<UserDetails />} /> {/* Parameterized route */}
+            </Routes>
+        </Router>
+    );
+};
 
-  return (
-    <>
-        <h1 className="underline font-bold text-center content-center mt-80">React App</h1>
-        <Footer />
-    </>
-  )
-}
-export default App
+export default App;
