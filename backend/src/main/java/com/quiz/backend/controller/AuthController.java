683afcc9c60681ab6196340d42fee542602ee635
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-10 18:08:12 +0530

    updated AuthController to handle login using jwt

--- backend/src/main/java/com/quiz/backend/controller/AuthController.java
+++ backend/src/main/java/com/quiz/backend/controller/AuthController.java
@@ -1,11 +1,16 @@
 package com.quiz.backend.controller;
 
 import com.quiz.backend.dto.LoginDTO;
-import com.quiz.backend.dto.UserDTO;
+import com.quiz.backend.jwt.JwtUtils;
 import com.quiz.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,18 +19,33 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
 
     @Autowired
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
     }
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
-        UserDTO user = userService.authenticate(loginDTO.getUsername(), loginDTO.getPassword());
-        if (user != null) {
-            return new ResponseEntity<>(user, HttpStatus.OK);
-        } else {
+        Authentication authentication;
+
+        try {
+            authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword())
+            );
+        } catch (Exception e) {
             return new ResponseEntity<>("Invalid credentials", HttpStatus.UNAUTHORIZED);
         }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+
+        return ResponseEntity.ok(jwtToken);
     }
 }
