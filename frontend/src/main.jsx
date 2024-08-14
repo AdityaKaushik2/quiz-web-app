e0ced7b5751daabe5feb585d6d7fc2ec7b66f28b
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-15 00:26:54 +0530

    configured react toastify

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,27 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css'
+import {ToastContainer} from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+                bodyClassName="toastBody"
+            />
         </Provider>
     </React.StrictMode>,
 );
