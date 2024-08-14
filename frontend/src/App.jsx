1f2c1bb5a57d44d59bae493dc94f2cdac22c7b27
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-15 00:05:11 +0530

    updated app.jsx and main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/LoginSection.jsx';
-import Register from './pages/Register';
-import UserDetails from './pages/UserDetailsPage.jsx';
-import Homepage from "./components/Homepage.jsx"; // Ensure this is the correct path
+import Dashboard from './components/Dashboard';
+import AddQuiz from './components/AddQuiz';
+import UpdateProfile from './components/UpdateProfile';
+import Quizzes from './components/Quizzes';
+import UpdateQuiz from './components/UpdateQuiz';
+import LoginPage from './components/LoginSection.jsx';
+import RegisterPage from './components/Register.jsx';
+import Questions from './components/Questions.jsx';
+import QuizCodeSection from "./components/QuizCodeSection.jsx";
+import AttemptQuiz from "./components/AttemptQuiz.jsx";
 
-const App = () => {
+function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/user-details/:username" element={<UserDetails />} /> {/* Parameterized route */}
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/add-quiz" element={<AddQuiz />} />
+                <Route path="/update-profile" element={<UpdateProfile />} />
+                <Route path="/quizzes" element={<Quizzes />} />
+                <Route path="/update-quiz/:quizId" element={<UpdateQuiz />} />
+                <Route path="/quiz/:quizId/questions" element={<Questions />} />
+                <Route path="/attempt-quiz" element={<QuizCodeSection />} />
+                <Route path="/attempt-quiz/:quizCode" element={<AttemptQuiz />} />
             </Routes>
         </Router>
     );
-};
+}
 
 export default App;
