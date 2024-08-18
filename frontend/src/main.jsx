5d0c1f66784588feeb889989c1b4fa9e498ef11b
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-08-18 11:03:23 +0530

    update notification time to 1 sec

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <App />
             <ToastContainer
                 position="top-center"
-                autoClose={3000}
+                autoClose={1000}
                 hideProgressBar={false}
                 newestOnTop={false}
                 closeOnClick
